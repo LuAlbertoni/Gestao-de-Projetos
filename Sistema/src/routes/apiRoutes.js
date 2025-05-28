648206a69232d8e0fd20d8383f7211785707671f
LuAlbertoni <lualbertoni@protonmail.com>
2025-05-28 20:29:50 -0300

    Adiciona lógica de autenticação e controle de sessão AB#143

--- Sistema/src/routes/apiRoutes.js
+++ Sistema/src/routes/apiRoutes.js
@@ -7,6 +7,8 @@ const router = express.Router();
 // Usuários
 router.post("/auth/register", authController.register);
 router.post("/auth/login", authController.login);
+router.get("/auth/me", authController.me);
+router.post("/auth/logout", authController.logout);
 
 // Receitas
 router.post("/receitas", receitaController.criar);
