709c9c798682f20ed09ee7f5d0938431e07f45af
NathalyLima <nathalylimasouza@gmail.com>
2025-06-03 21:01:45 -0300

    Adicionado as rotas das páginas de autenticação e inicial AB#227

--- Sistema/src/app.js
+++ Sistema/src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const apiRoutes = require("./routes/apiRoutes");
 const viewRoutes = require("./routes/viewRoutes");
@@ -9,6 +10,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/css", express.static(path.join(__dirname, "..", "public", "css")));
+app.use("/js", express.static(path.join(__dirname, "..", "public", "js")));
 
 app.use("/api", apiRoutes);
 app.use("/", viewRoutes);
--- Sistema/src/routes/viewRoutes.js
+++ Sistema/src/routes/viewRoutes.js
@@ -2,9 +2,18 @@ const express = require("express");
 const path = require("path");
 
 const router = express.Router();
+const publicRoot = path.resolve(process.cwd(), "public");
 
-router.get("/", (_, res) => {
-  res.send("Olá Mundo");
+router.get("/", (req, res) => {
+  res.sendFile(path.join(publicRoot, "index.html"));
+});
+
+router.get("/entrar", (req, res) => {
+  res.sendFile(path.join(publicRoot, "entrar.html"));
+});
+
+router.get("/cadastro", (req, res) => {
+  res.sendFile(path.join(publicRoot, "cadastro.html"));
 });
 
 module.exports = router;
